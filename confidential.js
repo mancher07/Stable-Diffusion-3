10ea38c481dea963f63d4001717218e79e18d048
mancher07 <64319877+mancher07@users.noreply.github.com>
2024-07-11 15:16:42 -0300

    Create confidential.js

---
+++ confidential.js
@@ -0,0 +1,1 @@
+echo "# Stable-Diffusion-3" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/mancher07/Stable-Diffusion-3.git git push -u origin main
